0a2ce3e66ecc5fbf7b0c657b3b50f6a880284c24
dolphinlmg <dolphinlmg@naver.com>
2024-02-08 18:51:39 +0900

    feat: eslint 추가 설정
    shared에 eslint 추가
    no-empty-interface 설정 추가

--- packages/.eslintrc.js
+++ packages/.eslintrc.js
@@ -5,39 +5,25 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:prettier/recommended',
-    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:@typescript-eslint/eslint-recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'plugin:@next/next/recommended',
     'plugin:import/typescript',
-    'plugin:storybook/recommended',
   ],
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  env: {
-    browser: true,
-    es6: true,
-  },
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
     ecmaFeatures: {
-      jsx: true, // Allows for the parsing of JSX
       arrowFunctions: true,
     },
   },
-  plugins: ['react', '@typescript-eslint', 'prettier', 'import', 'unused-imports'],
+  plugins: ['@typescript-eslint', 'prettier', 'import', 'unused-imports'],
   settings: {
-    react: {
-      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
-    },
     'import/resolver': {
       typescript: {},
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
-    },
-    next: {
-      rootDir: 'apps/baram-frontend',
     },
   },
   rules: {
@@ -50,18 +36,12 @@ module.exports = {
     'class-methods-use-this': 'off',
     'import/extensions': 'off',
     'import/prefer-default-export': 'off',
-    'react/display-name': 'off',
-    '@next/next/no-img-element': 'off',
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-empty-interface': 'error',
-    '@next/next/no-sync-scripts': 'off',
     // New rules
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-pascal-case': 'error',
     'no-nested-ternary': 'error',
     curly: 'error',
     'no-use-before-define': 'off',
@@ -80,26 +60,6 @@ module.exports = {
         groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
         pathGroups: [
           {
-            pattern: 'next/**',
-            group: 'builtin',
-            position: 'before',
-          },
-          {
-            pattern: 'react',
-            group: 'builtin',
-            position: 'before',
-          },
-          {
-            pattern: '@tanstack/**',
-            group: 'external',
-            position: 'before',
-          },
-          {
-            pattern: '@mui/**',
-            group: 'internal',
-            position: 'before',
-          },
-          {
             pattern: '@shared/**',
             group: 'internal',
             position: 'before',
@@ -114,7 +74,6 @@ module.exports = {
           order: 'asc',
           caseInsensitive: true,
         },
-        pathGroupsExcludedImportTypes: ['react'],
       },
     ],
     'unused-imports/no-unused-imports': 'error',
