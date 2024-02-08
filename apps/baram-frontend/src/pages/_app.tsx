9c767c052a0fb3506f644a38c2baaf73fe3bc8b4
dolphinlmg <dolphinlmg@naver.com>
2024-02-08 18:24:31 +0900

    feat: tsconfig path alias 설정

--- apps/baram-frontend/src/pages/_app.tsx
+++ apps/baram-frontend/src/pages/_app.tsx
@@ -1,8 +1,8 @@
+import type { AppProps } from 'next/app';
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-import type { AppProps } from 'next/app';
-import '@/styles/fonts';
-import '@/styles/global.css';
+import '@src/styles/fonts';
+import '@src/styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
